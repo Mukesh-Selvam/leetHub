c817e7c94de2e3d8750689a36d216db68932e5aa
MUKESH S <selvamsrmk@gmail.com>
2026-07-03 13:32:40 +0530

    Time: 853 ms (10.79%), Space: 11.8 MB (77.35%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int count=0,ans=0;
+        findsum(0,nums,target,count,ans);
+        return ans;
+    }
+    void findsum(int i,vector<int>& nums,int target,int count,int& ans){
+        if(i==nums.size()){
+            if(count==target) ans++;
+            return;
+        }
+        count+=nums[i];
+        findsum(i+1,nums,target,count,ans);
+        count-=(2*nums[i]);
+        findsum(i+1,nums,target,count,ans);
+    }
+};
