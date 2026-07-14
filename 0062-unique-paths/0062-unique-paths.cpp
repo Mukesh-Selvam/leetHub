54d2253d72bb1eb656649f32311e7eea983ecf79
MUKESH S <selvamsrmk@gmail.com>
2026-07-14 09:50:01 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution{
 public:
     int uniquePaths(int m, int n) {
@@ -9,5 +10,21 @@ public:
     }
 
     return (int)res;
+    }
+};
+*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return paths(m-1,n-1,dp);
+    }
+    int paths(int i,int j,vector<vector<int>>& dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int l=paths(i,j-1,dp);
+        int u=paths(i-1,j,dp);
+        return dp[i][j]=l+u;
     }
 };
