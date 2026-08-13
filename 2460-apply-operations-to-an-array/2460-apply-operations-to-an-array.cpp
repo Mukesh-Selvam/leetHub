ccdcb5122c4386c0422dff4ecf68c0ccf1f763e0
MUKESH S <selvamsrmk@gmail.com>
2026-08-13 14:26:20 +0530

    Time: 0 ms (100%), Space: 13.1 MB (64.95%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        int writeIdx = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (i < n - 1 && nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+            if (nums[i] != 0) {
+                swap(nums[writeIdx++], nums[i]);
+            }
+        }
+        return nums;
+    }
+};
